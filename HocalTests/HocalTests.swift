6e0ac40fdb18ef6033489b9fa2a8920f80b16856
arav <aravsachdevajorge@gmail.com>
2025-01-07 15:50:14 -0500

    Initial Commit

---
+++ HocalTests/HocalTests.swift
@@ -0,0 +1,17 @@
+//
+//  HocalTests.swift
+//  HocalTests
+//
+//  Created by Arav Sachdeva on 1/7/25.
+//
+
+import Testing
+@testable import Hocal
+
+struct HocalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
